8def3b0e9bb14affd9fb04e0447139c8be4f89df
Christopher Gooley <c@thegooley.com>
2013-09-28 11:11:35 -0700

    some clients

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'riak'
+gem 'redis'
+gem 'json'
--- demo.rb
+++ demo.rb
@@ -1,21 +1,40 @@
 require 'json'
+require 'redis'
 
-people = {}
-event_count = 0
-people_counts = Hash.new(0)
-
-while line = gets
-  data = JSON.parse(line)
-  p = data["person"]
-  email = p["email"]
-  people[email] = p
-  people_counts[email] += 1
-  event_count += 1
+def redis
+  @redis_client ||= Redis.new(:host => "192.168.1.10", :database => 7)
 end
 
-people_counts.sort_by{ |k,v| v * -1 }.each do |k,v|
-  puts "#{v}\t#{k}"
+def riak
+  @riak_client ||= Riak::Client.new(:nodes => [
+        {:host => 'ec2-50-16-172-76.compute-1.amazonaws.com'},
+        {:host => 'ec2-54-243-23-98.compute-1.amazonaws.com'},
+        {:host => 'ec2-54-221-81-11.compute-1.amazonaws.com'},
+        {:host => 'ec2-54-226-94-108.compute-1.amazonaws.com'},
+        {:host => 'ec2-184-72-173-119.compute-1.amazonaws.com'}
+      ])
 end
 
-puts "found #{people.count} total people"
-puts "found #{event_count} total events"
+def process
+  people = {}
+  event_count = 0
+  people_counts = Hash.new(0)
+
+  while line = gets
+    data = JSON.parse(line)
+    p = data["person"]
+    email = p["email"]
+    people[email] = p
+    people_counts[email] += 1
+    event_count += 1
+  end
+
+  people_counts.sort_by{ |k,v| v * -1 }.each do |k,v|
+    puts "#{v}\t#{k}"
+  end
+
+  puts "found #{people.count} total people"
+  puts "found #{event_count} total events"
+end
+
+process
